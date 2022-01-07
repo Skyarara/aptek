c8a65628584e400ed27599f39da1c0f8a15ff417
Richard Tandy Japutra <richardtandy2015@gmail.com>
2022-01-07 22:52:51 +0700

    type produk

--- pages/TipeProduk/index.php
+++ pages/TipeProduk/index.php
@@ -1,21 +1,10 @@
-<!--
-=========================================================
-* Material Dashboard 2 - v3.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <?php
 
-require 'fungsiadmin.php';
-require 'cekAdmin.php';
+require '../fungsiadmin.php';
+// require '../cekAdmin.php';
+
+$sql = "SELECT * FROM typeproduk";
+$query = mysqli_query($conn, $sql);
 
 ?>
 <!DOCTYPE html>
@@ -30,26 +19,31 @@ require 'cekAdmin.php';
     Admin Dashboard
   </title>
   <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <link rel="stylesheet" type="text/css"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="../../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../../assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <!-- Material Icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
   <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/material-dashboard.css?" rel="stylesheet" />
+  <link id="pagestyle" href="../../assets/css/material-dashboard.css?" rel="stylesheet" />
   <!-- table -->
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js">
+  </script>
 
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
-  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
+  <aside
+    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
+    id="sidenav-main">
     <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+        aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="#" target="_blank">
         <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold text-white">HewoDocc</span>
@@ -82,7 +76,7 @@ require 'cekAdmin.php';
             <span class="nav-link-text ms-1">Transaksi Keluar</span>
           </a>
         </li>
-        
+
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
@@ -92,7 +86,8 @@ require 'cekAdmin.php';
   </aside>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
+      navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -120,99 +115,89 @@ require 'cekAdmin.php';
     </nav>
     <!-- End Navbar -->
     <div class="container-fluid py-4">
-      
-        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table mr-1"></i>
-                                Daftar Produk
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                        <thead>
-                                            <tr>
-                                                <th>ID Produk</th>
-                                                <th>ID Kategori</th>
-                                                <th>Nama Produk</th>
-                                                <th>Harga Produk</th>
-                                                <th>Stok</th>
-                                            </tr>
-                                        </thead>
-                                        <tfoot>
-                                            <tr>
-                                                <th>ID Produk</th>
-                                                <th>ID Kategori</th>
-                                                <th>Nama Produk</th>
-                                                <th>Harga Produk</th>
-                                                <th>Stok</th>
-                                            </tr>
-                                        </tfoot>
-                                        <tbody>
-                                            <tr>
-                                                <th>1</th>
-                                                <th>1</th>
-                                                <th>Ultra Flu</th>
-                                                <th>Rp. 12.000</th>
-                                                <th>513</th>
-                                            </tr>
-                                            <tr>
-                                                <th>2</th>
-                                                <th>1</th>
-                                                <th>OBH Combi</th>
-                                                <th>Rp. 29.000</th>
-                                                <th>219</th>
-                                            </tr>
-                                            <tr>
-                                                <th>3</th>
-                                                <th>2</th>
-                                                <th>Entrostop</th>
-                                                <th>Rp. 23.500</th>
-                                                <th>878</th>
-                                            </tr>
-                                            <tr>
-                                                <th>4</th>
-                                                <th>3</th>
-                                                <th>Paracetamol Untuk Anak</th>
-                                                <th>Rp. 50.000</th>
-                                                <th>654</th>
-                                            </tr>
-                                            <tr>
-                                                <th>5</th>
-                                                <th>3</th>
-                                                <th>Panadol</th>
-                                                <th>Rp. 14.000</th>
-                                                <th>455</th>
-                                            </tr>
-                                            
-                                            
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-        
+
+      <div class="card mb-4">
+        <div class="card-header">
+          <i class="fas fa-table mr-1"></i>
+          Daftar Produk
+        </div>
+        <div class="card-body">
+          <form action='tambah.php' method='POST'>
+            <div class="form-grop row">
+              <div class="col">
+                <div class="input-group input-group-dynamic mb-4 col-xs-2">
+                  <label class="form-label">Kode Obat</label>
+                  <input type="text" name='kode' class="form-control" required>
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-group input-group-dynamic mb-4 col-xs-2">
+                  <label class='form-label'>Nama Obat</label>
+                  <input type="text" name='nama' class="form-control" required>
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Tambah</button>
+          </form>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>Nomor</th>
+                <th>Kode Type</th>
+                <th>Nama Produk</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Nomor</th>
+                <th>Kode Type</th>
+                <th>Nama Tipe</th>
+                <th>Aksi</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              <?php $a = 1; while($data = mysqli_fetch_array($query)): ?>
+              <tr>
+                <td><?= $a++ ?></td>
+                <td><?= $data['idType'] ?></td>
+                <td><?= $data['nama'] ?></td>
+                <td>
+                  <a href="delete.php?id=<?=$data['idType']?>" class='btn btn-danger'>Hapus</a>
+                  <a href="" class='btn btn-warning'>Ubah</a>
+                </td>
+              </tr>
+              <?php endwhile; ?>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <footer class="footer py-4  ">
-        <div class="container-fluid">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-            </div>
-            <div class="col-lg-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-              </ul>
-            </div>
+    </div>
+
+    </div>
+    <footer class="footer py-4  ">
+      <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6 mb-lg-0 mb-4">
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            </ul>
           </div>
         </div>
-      </footer>
+      </div>
+    </footer>
     </div>
   </main>
-  
+
   <!--   Core JS Files   -->
   <script src="../../assets/js/core/popper.min.js"></script>
   <script src="../../assets/js/core/bootstrap.min.js"></script>
   <script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="../../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  
+
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -225,12 +210,12 @@ require 'cekAdmin.php';
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/material-dashboard.min.js?v=3.0.0"></script>
+  <script src="../../assets/js/material-dashboard.min.js?v=3.0.0"></script>
 
   <!-- table -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
   <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-  <script src="../assets/js/dataTable.js"></script>
+  <script src="../../assets/js/dataTable.js"></script>
 </body>
 
 </html>
