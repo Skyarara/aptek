88ea6e79bfaaf95b2ef1c0901248d85cb39ab569
Nicolaus Avenito <86889601+LoweGule@users.noreply.github.com>
2022-01-08 22:35:03 +0700

    sistem login

--- pages/sign-in.php
+++ pages/sign-in.php
@@ -6,21 +6,20 @@ error_reporting(0);
  
 session_start();
  
-if (isset($_SESSION['username'])) {
-  header("Location: loginsuccess.php");
+if (isset($_SESSION['nama'])) {
+  header("Location: dashboard.html");
 }
 
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = md5($_POST['password']);
  
-    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $sql = "SELECT * FROM customer WHERE email='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
-        $_SESSION['username'] = $row['username'];
-        header("Location: berhasil_login.php");
-        header("Location: loginsuccess.php");
+        $_SESSION['nama'] = $row['nama'];
+        header("Location: dashboard.html");
     } else {
         echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
     }
@@ -79,18 +78,18 @@ if (isset($_POST['submit'])) {
                 <form role="form" class="text-start" action="" method="POST" class="login-email">
                   <div class="input-group input-group-outline my-3">
                     <label class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email" value="<?php echo $email; ?>" required>
+                    <input type="email" id="email" class="form-control" name="email" required>
                   </div>
                   <div class="input-group input-group-outline mb-3">
                     <label class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" value="<?php echo $_POST['password']; ?>" required>
+                    <input type="password" id="password" class="form-control" name="password" required>
                   </div>
                   <div class="form-check form-switch d-flex align-items-center mb-3">
                     <input class="form-check-input" type="checkbox" id="rememberMe">
                     <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember me</label>
                   </div>
                   <div class="text-center">
-                    <button type="button" name="submit"class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                    <button type="submit" id="submit" name="submit" value="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
                   </div>
                   <p class="mt-4 text-sm text-center">
                     Don't have an account?
